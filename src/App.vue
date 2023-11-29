0de5595d48b6b92f46cb55da4f352587065751b3
navin <navin.joshua@greenhonchos.com>
2023-11-29 15:45:06 +0530

    test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+   <SiteNavigation />
+   <HomeView></HomeView>
    <RouterView></RouterView>
   </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import HomeView from './components/homeView.vue';
+import SiteNavigation from './components/siteNavigation.vue';
 
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,26 @@ module.exports = {
   // This is to tell tailwind where to look for classes that we are applying in this project
   content: [
     "/index.html", // Tailwind will look for our classes in index.html file
-    "/src/**/*.{vue,js,ts,jsx,tsx}", // Looking for files with the specified extensions (vue, js, ts, jsx, tsx) in any subdirectory under the "/src/" directory.
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Looking for files with the specified extensions (vue, js, ts, jsx, tsx) in any subdirectory under the "/src/" directory.
   ],
     theme: {
     extend: {
       colors:{
-        "weather-primary":"00668A",
-        "weather-secondary":"004E71"
-      }
+        "weather-primary":"#00668A",
+        "weather-secondary":"#004E71"
+      },
     },
+    fontFamily:{
+      Roboto:["Roboto, sans-senrif"]
+    },
+    container:{
+      padding: '2rem',
+      center: true
+    },
+    screens:{
+      sm: '640px',
+      md: '768px'
+    }
   },
   plugins: [],
 }
